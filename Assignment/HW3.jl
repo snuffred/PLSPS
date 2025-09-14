01e80dc33d4286b0b6cce47877b5a13db6a030a9
Snuff-red <Snuff-red1900@outlook.com>
2025-09-14 13:59:59 +0200

    HW3 and test

--- Assignment/HW3.jl
+++ Assignment/HW3.jl
@@ -1,17 +1,29 @@
 using Distributed
 
+n = 5
+addprocs(n)
+
 f = () -> Channel{Int}(1)
 worker_ids = workers()
 chnls = [RemoteChannel(f, w) for w in worker_ids]
+
 @sync for (iw, w) in enumerate(worker_ids)
-    @spawnt w begin
-        chnl_snd = chnls[iw]
-        if iw == 1
-            chnl_rcv = chnls[end]
-            msg = 2
-            println("msg  = $msg")
-            put!(chnl_snd, msg)
-        end
-    end
+	@spawnat w begin
+		chnl_snd = chnls[iw]
+		if iw == 1
+			chnl_rcv = chnls[end]
+			msg = 2
+			println("msg=$msg")
+			put!(chnl_snd, msg)
+			msg = take!(chnl_rcv)
+			println("msg=$msg")
+		else
+			chnl_rcv = chnls[iw-1]
+			msg = take!(chnl_rcv)
+			msg += 1
+			println("msg=$msg")
+			put!(chnl_snd, msg)
+		end
+	end
 end
 
